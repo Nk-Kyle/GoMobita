b51c29845adbda8fa9ff4e02a792d178325f9d52
fayzanadia@gmail.com <fayzanadia@gmail.com>
2021-10-24 21:38:56 +0700

    add list gadget

---
+++ app/ADTList/listGadget.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include "listGadget.h"
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create List kosong  */
+void CreateListGadget(ListGadget *l)
+/* I.S. l sembarang */
+/* F.S. Terbentuk List l kosong dengan kapasitas CAPACITY */
+/* Proses: Inisialisasi semua elemen List l dengan VAL_UNDEF */
+{
+    for (int i = 0; i < LISTGADGET_CAPACITY; i++)
+    {
+        ELMT(*l, i) = VAL_UNDEF;
+    }
+}
+
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* ********** Test Indeks yang valid ********** */
+boolean isIdxGadgetEff(ListGadget l, int i)
+/* Mengirimkan true jika i adalah indeks yang terdefinisi utk List l */
+{
+    return (ELMT(l, i) != VAL_UNDEF);
+}
+
+/* ********** TEST KOSONG/PENUH ********** */
+/* *** Test List kosong *** */
+boolean isGadgetEmpty(ListGadget l)
+/* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
+{
+    int i = 0;
+
+    while (i < LISTGADGET_CAPACITY)
+    {
+        if (isIdxGadgetEff(l, i))
+        {
+            return false;
+        }
+        i++;
+    }
+
+    return true;
+}
+/* *** Test List penuh *** */
+boolean isGadgetFull(ListGadget l)
+/* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
+{
+    int i = 0;
+
+    while (i < LISTGADGET_CAPACITY)
+    {
+        if (!isIdxGadgetEff(l, i))
+        {
+            return false;
+        }
+        i++;
+    }
+
+    return true;
+}
+
+/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
+/* *** Mendefinisikan isi List dari pembacaan *** */
+void displayListGadget(ListGadget l)
+/* Proses : Menuliskan isi List dengan traversal */
+/* I.S. l boleh kosong */
+/* F.S. l ditampilkan ke layar */
+{
+    for (int i = 0; i < LISTGADGET_CAPACITY; i++)
+    {
+        printf("%d. ", (i + 1));
+        if (ELMT(l, i) == 1)
+        {
+            printf("Kain Pembungkus Waktu\n");
+        }
+        else if (ELMT(l, i) == 2)
+        {
+            printf("Senter Pembesar\n");
+        }
+        else if (ELMT(l, i) == 3)
+        {
+            printf("Pintu Kemana Saja\n");
+        }
+        else if (ELMT(l, i) == 4)
+        {
+            printf("Mesin Waktu\n");
+        }
+        else
+        {
+            printf("-\n");
+        }
+    }
+}
+
+/* ********** SEARCHING ********** */
+/* ***  Perhatian : List boleh kosong!! *** */
+int indexOf(ListGadget l, ElType val)
+/* Search apakah ada elemen List l yang bernilai val */
+/* Jika ada, menghasilkan indeks i terkecil, dengan ELMT(l,i) = val */
+/* Jika tidak ada atau jika l kosong, mengirimkan IDX_UNDEF */
+/* Skema Searching yang digunakan bebas */
+{
+    int i = 0;
+
+    while (i < LISTGADGET_CAPACITY)
+    {
+        if (ELMT(l, i) == val)
+        {
+            return i;
+        }
+        i++;
+    }
+
+    return IDX_UNDEF;
+}
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN ********** */
+/* *** Menambahkan elemen *** */
+void insertGadget(ListGadget *l, ElType val)
+/* Proses: Menambahkan val sebagai elemen List */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen l yang baru */
+{
+    int i = 0;
+    int idxInsert;
+    boolean found = false;
+
+    if (!isGadgetFull(*l))
+    {
+        while ((i < LISTGADGET_CAPACITY) && !found)
+        {
+            if (!isIdxGadgetEff(*l, i))
+            {
+                idxInsert = i;
+                found = true;
+            }
+            i++;
+        }
+
+        ELMT(*l, idxInsert) = val;
+    }
+}
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteGadget(ListGadget *l, ElType *val)
+/* Proses : Menghapus elemen List */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen l sebelum penghapusan, */
+/*      Banyaknya elemen List berkurang satu */
+/*      List l mungkin menjadi kosong */
+{
+    int i = 0;
+    int idxDelete = indexOf(*l, *val);
+
+    *val = ELMT(*l, idxDelete);
+    ELMT(*l, idxDelete) = VAL_UNDEF;
+}
---
+++ app/ADTList/listGadget.h
@@ -0,0 +1,84 @@
+/* MODUL LIST INTEGER DENGAN ELEMEN POSITIF */
+/* Berisi definisi dan semua primitif pemrosesan list integer dengan elemen positif */
+/* Banyaknya elemen didefinisikan secara implisit, memori array statik */
+
+#ifndef LISTGADGET_H
+#define LISTGADGET_H
+
+#include "../boolean.h"
+
+/*  Kamus Umum */
+#define LISTGADGET_CAPACITY 5
+/* Kapasitas penyimpanan */
+#define IDX_UNDEF -1
+/* Indeks tak terdefinisi*/
+#define VAL_UNDEF -999
+/* Nilai elemen tak terdefinisi*/
+
+/* Definisi elemen dan koleksi objek */
+typedef int ElType;  /* type elemen List */
+typedef struct {
+   ElType contents[LISTGADGET_CAPACITY]; /* memori tempat penyimpan elemen (container) */
+} ListGadget;
+/* Indeks yang digunakan [0..CAPACITY-1] */
+/* Jika l adalah ListGadget, cara deklarasi dan akses: */
+/* Deklarasi : l : ListGadget */
+/* Maka cara akses:
+   ELMT(l,i) untuk mengakses elemen ke-i */
+/* Definisi :
+   List kosong: semua elemen bernilai VAL_UNDEF
+   Definisi elemen pertama: ELMT(l,i) dengan i=0 */
+
+/* ********** SELEKTOR ********** */
+#define ELMT(l, i) (l).contents[(i)]
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create List kosong  */
+void CreateListGadget(ListGadget *l);
+/* I.S. l sembarang */
+/* F.S. Terbentuk List l kosong dengan kapasitas CAPACITY */
+/* Proses: Inisialisasi semua elemen List l dengan VAL_UNDEF */
+
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* ********** Test Indeks yang valid ********** */
+boolean isIdxGadgetEff(ListGadget l, int i);
+/* Mengirimkan true jika i adalah indeks yang terdefinisi utk List l */
+
+/* ********** TEST KOSONG/PENUH ********** */
+/* *** Test List kosong *** */
+boolean isGadgetEmpty(ListGadget l);
+/* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
+/* *** Test List penuh *** */
+boolean isGadgetFull(ListGadget l);
+/* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
+
+/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
+/* *** Mendefinisikan isi List dari pembacaan *** */
+void displayListGadget(ListGadget l);
+/* Proses : Menuliskan isi List dengan traversal */
+/* I.S. l boleh kosong */
+/* F.S. l ditampilkan ke layar */
+
+/* ********** SEARCHING ********** */
+/* ***  Perhatian : List boleh kosong!! *** */
+int indexOf(ListGadget l, ElType val);
+/* Search apakah ada elemen List l yang bernilai val */
+/* Jika ada, menghasilkan indeks i terkecil, dengan ELMT(l,i) = val */
+/* Jika tidak ada atau jika l kosong, mengirimkan IDX_UNDEF */
+/* Skema Searching yang digunakan bebas */
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN ********** */
+/* *** Menambahkan elemen *** */
+void insertGadget(ListGadget *l, ElType val);
+/* Proses: Menambahkan val sebagai elemen terakhir List */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yang baru */
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteGadget(ListGadget *l, ElType *val);
+/* Proses : Menghapus elemen terakhir List */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen List berkurang satu */
+/*      List l mungkin menjadi kosong */
+
+#endif
